1d284a6f56704f736928a1e00c88d0ed1494c569
dodiameer <mtxshiftg@gmail.com>
2021-06-29 18:53:24 +0300

    added purgecss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,10 @@
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
+/**
+ * @type {import("@fullhuman/postcss-purgecss").default}
+ */
+// @ts-ignore
+const purgecss = require("@fullhuman/postcss-purgecss")
 
 const mode = process.env.NODE_ENV;
 const dev = mode === "development";
@@ -7,7 +12,9 @@ const dev = mode === "development";
 const config = {
 	plugins: [
 		autoprefixer(),
-
+		!dev && purgecss({
+			content: ["./src/**/*.html", "./src/**/*.svelte"]
+		}),
 		!dev && cssnano({
 			preset: "default",
 		}),
